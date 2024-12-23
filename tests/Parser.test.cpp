2417874c99b0d21664bb2cbc1239ffdf6635e4a2
Somē Cho <itssomicho@gmail.com>
2024-12-23 11:47:02 +0100

    refactor Parser.test.cpp
    Parameterize and separate concerns

--- tests/Parser.test.cpp
+++ tests/Parser.test.cpp
@@ -1,28 +1,58 @@
 #include <gtest/gtest.h>
 #include <string>
+#include <unordered_map>
 
+#include "MidiParser/MidiFile.hpp"
 #include "MidiParser/Parser.hpp"
 
-TEST(Parser_ParsesFile, Queen) {
+class Parser : public testing::TestWithParam<std::string> {
+ public:
+  Parser() {};
+
+  static void SetUpTestSuite() {
+    map = new std::unordered_map<std::string, MidiParser::MidiFile>();
+  };
+
+  static void TearDownTestSuite() {
+    delete map;
+    map = nullptr;
+  };
+
+  std::string data = std::string(EXAMPLES_DIR) + "/" + GetParam() + ".mid";
+  static std::unordered_map<std::string, MidiParser::MidiFile>* map;
+};
+
+std::unordered_map<std::string, MidiParser::MidiFile>* Parser::map = nullptr;
+
+TEST_P(Parser, ParsesFileWithoutThrowing) {
   auto p = MidiParser::Parser();
-  auto f = p.parse(std::string(EXAMPLES_DIR) + "/queen.mid");
-  ASSERT_EQ(f.numTracks, f.tracks.size());
+  auto m = p.parse(data);
+  map->insert({GetParam(), m});
 }
 
-TEST(Parser_ParsesFile, Mozart) {
-  auto p = MidiParser::Parser();
-  auto f = p.parse(std::string(EXAMPLES_DIR) + "/mozart.mid");
-  ASSERT_EQ(f.numTracks, f.tracks.size());
+TEST_P(Parser, HeaderNumMatchesNumParsedTracks) {
+  const auto& m = map->at(GetParam());
+  EXPECT_EQ(m.numTracks, m.tracks.size());
 }
 
-TEST(Parser_ParsesFile, Mahler) {
-  auto p = MidiParser::Parser();
-  auto f = p.parse(std::string(EXAMPLES_DIR) + "/mahler.mid");
-  ASSERT_EQ(f.numTracks, f.tracks.size());
+TEST_P(Parser, ParsedTracksAreNotEmpty) {
+  const auto& m = map->at(GetParam());
+  for (const auto& t : m.tracks) {
+    EXPECT_FALSE(t.events.empty());
+  }
 }
 
-TEST(Parser_ParsesFile, Debussy) {
-  auto p = MidiParser::Parser();
-  auto f = p.parse(std::string(EXAMPLES_DIR) + "/debussy.mid");
-  ASSERT_EQ(f.numTracks, f.tracks.size());
+TEST_P(Parser, SizesReadMatchesRealSize) {
+  const auto& m = map->at(GetParam());
+  size_t s = 14;
+  for (const auto& t : m.tracks) {
+    s += t.length + 8;
+  }
+  std::ifstream file(data, std::ios::binary);
+  file.seekg(0, std::ios::end);
+  EXPECT_EQ(s, file.tellg());
 }
+
+INSTANTIATE_TEST_SUITE_P(
+    Basic, Parser, testing::Values("queen", "mozart", "debussy", "mahler"),
+    [](const testing::TestParamInfo<std::string>& info) { return info.param; });
